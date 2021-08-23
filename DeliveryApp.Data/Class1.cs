15df3055d9e288e0683da6c66eb17e6abcd73545
jempool <jem.suarez@jalasoft.com>
2021-08-23 17:11:45 -0500

    Adding a 3-layer folder structure: Data, Logic and Presentation

---
+++ DeliveryApp.Data/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace DeliveryApp.Data
+{
+    public class Class1
+    {
+    }
+}
---
+++ DeliveryApp.Logic/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace DeliveryApp.Logic
+{
+    public class Class1
+    {
+    }
+}
